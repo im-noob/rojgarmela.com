8a6a50b4f878c77f8e3546d715f01d4f96547c48
nishant <nishantraj656@gmail.com>
2018-01-10 16:29:51 +0530

    commit 2

--- resources/views/Rogarmela_Registration/rojgarmela_student_registration.blade.php
+++ resources/views/Rogarmela_Registration/rojgarmela_student_registration.blade.php
@@ -2,30 +2,44 @@
 <html lng="en">
   <head>
     <meta charset="UTF-8">
-    <title>Rojarmela.com:mainpage</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rojarmela.com: mainpage</title>
     <link href="{{url('/')}}/boot/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="{{url('/')}}/css/body_css.css" rel="stylesheet" type="text/css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
 
   </head>
 
-  <body class="body">
+  <body >
     
-         <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
               <a class="navbar-brand" href="#">Rojgarmela.com</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+              <!-- Toggler/collapsibe Button -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
               <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">About US</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Contect US</a>
+                  </li> 
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Apply</a>
                   </li>    
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Notice</a>
+                  </li> 
                 </ul>
               </div>  
             </nav>
