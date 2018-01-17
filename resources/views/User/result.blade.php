6d0f8668be1c6c162f11fea24013b7077c2294f2
im-noob <aaravonly4you@gmail.com>
2018-01-17 15:14:59 +0530

    wise swing editor added

--- resources/views/User/result.blade.php
+++ resources/views/User/result.blade.php
@@ -281,14 +281,14 @@ ga('send', 'pageview');
 <script defer src="https://daveismyname.blog/default/assets/js/main.js"></script>
 
 <script>
-var sc_project=10651444;
-var sc_invisible=1;
-var sc_security="13781b82";
-var sc_https=1;
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+"statcounter.com/counter/counter.js'></"+"script>");
+	var sc_project=10651444;
+	var sc_invisible=1;
+	var sc_security="13781b82";
+	var sc_https=1;
+	var scJsHost = (("https:" == document.location.protocol) ?
+	"https://secure." : "http://www.");
+	document.write("<sc"+"ript type='text/javascript' src='" +
+	scJsHost+"statcounter.com/counter/counter.js'></"+"script>");
 </script>
 @endsection
 
